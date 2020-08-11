f4462c6602693b1f6171585965e375f984a4207f
Flora Petersen <floramarina@comcast.net>
2020-08-11 23:30:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -65,7 +65,7 @@ class Dog
   end
 
   def self.find_or_create_by(name:, breed:)
-    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?")
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
     if !dog.empty?
       dog_attributes = dog[0]
       initialized_dog = Dog.new(dog_attributes[0], dog_attributes[1], dog_attributes[2])
